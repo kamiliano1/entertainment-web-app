9f692d22d03f0f5614b573df63c4e2f96f471191
kamiliano1 <kamiliano1991@gmail.com>
2023-03-19 20:33:50 +0100

    App

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { PageContext, PageNameType } from "@/src/Context";
-import { auth } from "@/src/firebase/clientApp";
+import { auth, firestore } from "@/src/firebase/clientApp";
 import { Flex, Icon, Image, Stack, VisuallyHidden } from "@chakra-ui/react";
+import { doc, getDoc } from "firebase/firestore";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useContext, useEffect, useState } from "react";
@@ -13,13 +14,44 @@ import { SiWindows11 } from "react-icons/si";
 import LoginModal from "../Modal/Modal";
 type NavbarProps = {};
 const Navbar: React.FC<NavbarProps> = () => {
-  const { setIsOpen, currentTab, setCurrentTab, avatarURL } =
-    useContext(PageContext);
+  const {
+    setIsOpen,
+    currentTab,
+    setCurrentTab,
+    avatarURL,
+    setBookmarkTitle,
+    setAvatarURL,
+  } = useContext(PageContext);
   const [user] = useAuthState(auth);
   const router = useRouter();
   const currentPageAddress = router.pathname.split("/")[1] as PageNameType;
   const [isLogged, setIsLogged] = useState(false);
+  const [loadingDatabase, setLoadingDataBase] = useState(false);
 
+  useEffect(() => {
+    const getUserData = async () => {
+      if (loadingDatabase === false) {
+        try {
+          const userDataRef = doc(firestore, "users", user!.uid);
+          const userData = await getDoc(userDataRef);
+          const bookmarkData = userData.data();
+
+          if (bookmarkData) {
+            setBookmarkTitle!(bookmarkData.bookmarkList);
+            setAvatarURL!(bookmarkData.avatar);
+            console.log("bookmarkData.avatar", bookmarkData.avatar);
+          }
+        } catch (error: any) {
+          console.log("getBookmarkError", error.message);
+        }
+      }
+      setLoadingDataBase(true);
+    };
+    if (user && !loadingDatabase) getUserData();
+    setLoadingDataBase(true);
+  }, [loadingDatabase, setAvatarURL, user, setBookmarkTitle]);
+
+  // if (user) getUserData();
   useEffect(() => {
     user ? setIsLogged(true) : setIsLogged(false);
   }, [user]);
